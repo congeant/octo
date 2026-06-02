553c5358cf9153e1e3852350b8c392352325cacf
vguerato <vguerato@gmail.com>
2026-06-01 21:16:11 -0300

    chore: bump to 0.5.2

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name('octo')
   .description('Monorepo build orchestration, versioning, and infrastructure CLI')
-  .version('0.5.1');
+  .version('0.5.2');
 
 program
   .command('init')
