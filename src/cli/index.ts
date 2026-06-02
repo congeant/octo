5e1c8e8ce3b0efb0c02307891e4bacb400e7e74c
vguerato <vguerato@gmail.com>
2026-06-01 22:49:19 -0300

    chore: bump to 0.8.0

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name('octo')
   .description('Build orchestration, semantic versioning, and local infrastructure management for repository workspaces')
-  .version('0.7.2');
+  .version('0.8.0');
 
 program
   .command('init')
